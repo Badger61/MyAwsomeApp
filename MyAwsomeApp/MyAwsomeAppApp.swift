690bcc48a5c071f3d7a1b47910a9ed9ee9965bb4
Harvey Rhody <harvey.rhody@gmail.com>
2024-10-31 14:11:20 -0400

    Initial Commit

---
+++ MyAwsomeApp/MyAwsomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAwsomeAppApp.swift
+//  MyAwsomeApp
+//
+//  Created by Harvey E Rhody on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct MyAwsomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
